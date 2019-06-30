eb4d7e39506de08a3d257b86550e25e4a8cab11e
Johannes <h-annie@live.co.za>
2019-06-30 11:39:59 +0200

    Removed old data classes

--- src/main/kotlin/markets/Ticker.kt
+++ src/main/kotlin/markets/Ticker.kt
@@ -19,23 +19,6 @@ class Ticker {
         val ticker: BasicTicker
     )
 
-    sealed class TickerTypes {
-        data class LunoTicker(val priceMovement: Double, val ticker: BasicTicker) : TickerTypes()
-        data class BinanceTicker(val priceMovement: Double, val ticker: BasicTicker) : TickerTypes()
-    }
-
-    data class TrackedTicker(
-        val timestamp: String,
-        val cryptoPair: Pair<TickerTypes.LunoTicker, TickerTypes.BinanceTicker>
-    )
-
-    data class OutputCryptoTicker(
-        val timeStamp: String? = Calendar.getInstance().time.toString(),
-        val basicTickers: ArrayList<BasicTicker> = arrayListOf(),
-        val zar: String = "",
-        val eur: String = ""
-    )
-
     data class FiatTicker(val base: String? = null, val date: String? = null, val rates: Map<String, Double>? = null)
 
     class Rates(val name: String = "", val value: Double = 0.0)
